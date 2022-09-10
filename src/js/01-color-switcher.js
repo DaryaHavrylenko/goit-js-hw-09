537e561e2322199a1114fb911b442f1ee41770ad
Darya <HavrylenkoIT@gmail.com>
2022-09-10 20:46:33 +0300

    edit task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,36 +1,28 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
 
-const timer = {
-    intervalId: null,
-    isActive: false,
-    start() {
-        if (this.isActive) {
-            return;
-        }
-        this.isActive = true;
-       this.intervalId = setInterval(() => {
-          let color = getRandomHexColor();
-  document.body.style.background = color;
-console.log(getRandomHexColor());
+btnStart.addEventListener('click', onStart);
+
+btnStop.addEventListener('click', onStop);
+
+function onStart() {
+      btnStart.disabled = true;
+      btnStop.disabled = false;
+ 
+    timerId = setInterval(() => {
+  let color = getRandomHexColor();
+    document.body.style.background = color;
   }, 1000);
-        },
-stop() {
-    clearInterval(this.intervalId);
-    this.isActive = false;
-        }
-    }
+}
 
-btnStart.addEventListener('click', () => {
-    timer.start();
-})
-   
-btnStop.addEventListener("click", () => {
-    timer.stop();
+function onStop() {
 
-});
-
+      btnStart.disabled = false;
+      btnStop.disabled = true;
+  clearInterval(timerId);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
